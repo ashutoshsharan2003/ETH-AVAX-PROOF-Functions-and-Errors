302ddb81a4ea140e95dcf437be1a8362e6d11cba
ashutoshsharan2003 <141537249+ashutoshsharan2003@users.noreply.github.com>
2024-06-19 09:10:16 +0530

    Final Update

--- TravelBooking.sol
+++ TravelBooking.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 contract TravelBooking {
 
   // Define an enum for different travel destinations
-  enum Destination { Paris, Tokyo, NewYork, Sydney }
+  enum Destination { Delhi, Mumbai, Banglore, Pune }
 
   // Structure to store travel booking details
   struct Booking {
